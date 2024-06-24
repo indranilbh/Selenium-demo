9578402bf5510b3b9fe15cde841b9b46cf74f80f
Indranil Bhattacharyya <indranil44u@gmail.com>
2024-06-24 21:13:32 +0530

    report

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -20,6 +20,7 @@ public class BasePage {
 
 	private String url;
 	private Properties prop;
+	public static String screenShotDestinationPath;
 
 	public BasePage() throws IOException {
 		prop = new Properties();
@@ -37,16 +38,28 @@ public class BasePage {
 		return url;
 	}
 
-	public void takeSnapShot(String name) throws IOException {
+	public static String takeSnapShot(String name) throws IOException {
 		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
 
-		File destFile = new File(System.getProperty("user.dir") + "\\target\\screenshots\\" + timestamp() + ".png");
-
-		FileUtils.copyFile(srcFile, destFile);
+		String destFile = System.getProperty("user.dir") + "\\target\\screenshots\\" + timestamp() + ".png";
+		screenShotDestinationPath = destFile;
+		
+		try {
+			FileUtils.copyFile(srcFile, new File(destFile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return name;
+		
 	}
 
-	public String timestamp() {
+	public static String timestamp() {
 		return new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(new Date());
+	}
+	
+	public static String getScreenshotDestinationPath() {
+		return screenShotDestinationPath;
 	}
 	
 	public static void waitForElementInvisible(WebElement element, Duration timer) throws IOException {
