da1d898a82eb90c40d6ea161fed6cf9a34eeb917
Indranil Bhattacharyya <indranil44u@gmail.com>
2024-06-25 08:59:44 +0530

    mobile

--- src/main/java/base/WebDriverInstance.java
+++ src/main/java/base/WebDriverInstance.java
@@ -2,6 +2,8 @@ package base;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -9,6 +11,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverInstance {
@@ -36,14 +39,30 @@ public class WebDriverInstance {
 
 		if (prop.getProperty("browser").equals("chrome")) {
 			ChromeOptions chromeOptions = new ChromeOptions();
-		    driver = new ChromeDriver(chromeOptions);		
-		 } else if (prop.getProperty("browser").equals("firefox")) {
+			if (prop.getProperty("mobile").equals("yes")) {
+				Map<String, Object> deviceMetrics = new HashMap<>();
+		        deviceMetrics.put("width", 375);
+		        deviceMetrics.put("height", 812);
+		        deviceMetrics.put("pixelRatio", 3.0);
+		        deviceMetrics.put("mobile", true);
+		        Map<String, Object> mobileEmulation = new HashMap<>();
+		        mobileEmulation.put("deviceMetrics", deviceMetrics);
+		        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+			}
 			
+		    driver = new ChromeDriver(chromeOptions);
+		    if(!prop.getProperty("mobile").equals("yes"))
+		    	driver.manage().window().maximize();
+		    
+		 } else if (prop.getProperty("browser").equals("edge")) {
+			 EdgeOptions edgeOptions =new EdgeOptions();
+			 driver = new EdgeDriver(edgeOptions);
+			 driver.manage().window().maximize();
 		} else {
 			
 		}
 
-		driver.manage().window().maximize();
+		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		return driver;
